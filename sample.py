9041ee3ea8f1f9d06ae47c9ba206bc83a8cc74d3
arunvijo <arunvijo2004@gmail.com>
2025-05-31 12:33:46 +0530

    Initial commit (fresh start)

---
+++ sample.py
@@ -0,0 +1,23 @@
+import math
+
+
+def calculate_circle_area(radius):
+    """Function calculate_circle_area."""
+    return math.pi * radius**2
+
+
+def unused_function():
+    """Function unused_function."""
+    return "This won't be used"
+
+
+class ExampleClass:
+    """Class ExampleClass."""
+
+    def __init__(self):
+        """Function __init__."""
+        self.value = 0
+
+    def increment(self):
+        """Function increment."""
+        self.value += 1
